78ac98337d2af940fb0ee5c99951e6da05ebf7cb
u1355256@umail.utah.edu <u1355256@umail.utah.edu>
2024-02-18 12:56:19 -0700

    geojson plotted, need to refine

--- main.js
+++ main.js
@@ -2,16 +2,16 @@ const pageWidth = window.innerWidth || document.documentElement.clientWidth || d
 const pageHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 function setup() {
-    const body = d3.select('body')
+    const body = d3.select('body');
 
     const header = body.append('div')
         .attr('class', 'header')
-        .attr("id", "header")
+        .attr("id", "header");
 
     const headerSvg = header.append("svg")
         .attr('id', 'headerSvg')
         .attr("height", 50)
-        .attr("width", pageWidth)
+        .attr("width", pageWidth);
 
     const text = d3.select("#headerSvg").append("text")
         .attr("class", "text title")
@@ -26,16 +26,39 @@ function setup() {
         .attr("y", 25);
 
     const maps_div = body.append('div')
-        // .attr('class', 'header')
-        .attr("id", "migration_div")
+        .attr("id", "migration_div");
 
     const migrationSvg = maps_div.append('svg')
         .attr('id', 'migrationSvg')
         .attr("height", pageHeight - 50)
-        .attr("width", pageWidth / 2)
-        
+        .attr("width", pageWidth / 2);
+
+    // Load GeoJSON data using fetch
+    fetch('plot_eargre_new.geojson')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(geojsonData => {
+            console.log('GeoJSON data:', geojsonData); // Check if GeoJSON data is printed in the console
+
+            // Create a D3 projection
+            const projection = d3.geoIdentity().fitSize([pageWidth / 2, pageHeight - 50], geojsonData);
+
+            // Create a path generator
+            const pathGenerator = d3.geoPath().projection(projection);
+
+            // Append paths for each MultiPolygon
+            migrationSvg.selectAll("path")
+                .data(geojsonData.features)
+                .enter().append("path")
+                .attr("d", pathGenerator)
+                .style("fill", "steelblue")
+                .style("stroke", "white");
+        })
+        .catch(error => console.error('Error loading GeoJSON file:', error));
 }
-
-
 
 setup();
