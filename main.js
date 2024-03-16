3f8a5d1bb2ec31a0824526eeaab4d227bc5daba0
u1355256@umail.utah.edu <u1355256@umail.utah.edu>
2024-03-16 12:12:48 -0600

    added functionality for pixels to change

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+const globalApplicationState = {
+    current_species : 'eargre',
+    current_state : 'ut_eg_geojsons',
+    current_species_data : null,
+    current_year_selection : null
+}
+
+globalApplicationState.current_species_data = `${globalApplicationState.current_state}/plot_${globalApplicationState.current_species}_2004`
+
+
 const pageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 const pageHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
@@ -33,33 +43,75 @@ function setup() {
         .attr("height", pageHeight - 50)
         .attr("width", pageWidth / 2);
 
+    // Append SVG for the slider
+    const sliderSvg = body.append('svg')
+        .attr('id', 'sliderSvg')
+        .attr("height", pageHeight)
+        .attr("width", pageWidth / 2)
+        .style("position", "absolute") // Position absolutely
+        .style("top", "50px") // Adjust top position as needed
+        .style("left", "0px") // Position it on the left side
+
+
+    function updateSpeciesData(year) {
+        globalApplicationState.current_species_data = `${globalApplicationState.current_state}/plot_${globalApplicationState.current_species}_${year}`;
+        console.log(globalApplicationState.current_species_data);
+    }  
+
+    // Create the slider
+    const slider = d3.sliderHorizontal()
+        .min(2004)
+        .max(2023)
+        .step(1)
+        .width(pageWidth / 2.5) // Adjust the width as needed
+        .tickValues(d3.range(2004, 2024, 1)) // Include all years as tick values
+        .tickFormat((d) => d) // Tick values range from 2004 to 2023
+        .on('onchange', val => {
+            updateSpeciesData(val)
+             updateMap(val);
+        });
+
+    // Append the slider to the slider SVG
+    sliderSvg.append('g')
+        .attr('transform', 'translate(60,510)') // Adjust positioning as needed
+        .attr("id", "slider_group")
+        .call(slider)
+        .attr('class', 'slider');
+
+    // Select all ticks and style them
+    sliderSvg.selectAll(".tick line")
+        .style("stroke-width", "2px") // Adjust stroke width as needed
+        .attr("y2", 10)
+        .style("stroke", "black");
+
+    // Create a D3 projection focusing on the US and Mexico
+    const projection = d3.geoAlbers()
+    .center([-5, 28])  // Center the map around the desired area
+    .scale(900)
+    .translate([pageWidth / 4, pageHeight / 2]);
+
+    // Define custom color scale for bird observation data
+    const customColorScale = d3.scaleSequential(d3.interpolateBlues)
+        .domain([-0.5, 1]);
+
+    // Create a path generator
+    const pathGenerator = d3.geoPath().projection(projection);
+
     // Load GeoJSON data for the entire United States and Mexico
     Promise.all([
         // C:\Users\u1355\OneDrive\Desktop\Data Viz\Thesis Website\birds\ut_eg_geojsons\plot_eargre_2004.geojson
         fetch('map_geojsons/custom.geo.json').then(response => response.json()), 
-        fetch('ut_eg_geojsons/plot_eargre_2005.geojson').then(response => response.json()),
+        fetch(`${globalApplicationState.current_species_data}.geojson`).then(response => response.json()),
         fetch('map_geojsons/us_states.geojson').then(response => response.json()), 
         fetch('map_geojsons/states.geojson').then(response => response.json()) 
     ])
+    
     .then(data => {
         const [usMexicoData, birdData, usStatesData, mexicoStatesData] = data;
         console.log('US-Mexico GeoJSON data:', usMexicoData);
         console.log('Bird population observation data:', birdData);
         console.log('US States GeoJSON data:', usStatesData);
         console.log('Mexico States GeoJSON data:', mexicoStatesData);
-
-        // Create a D3 projection focusing on the US and Mexico
-        const projection = d3.geoAlbers()
-            .center([-5, 28])  // Center the map around the desired area
-            .scale(900)
-            .translate([pageWidth / 4, pageHeight / 2]);
-
-        // Define custom color scale for bird observation data
-        const customColorScale = d3.scaleSequential(d3.interpolateBlues)
-            .domain([-0.5, 1]);
-
-        // Create a path generator
-        const pathGenerator = d3.geoPath().projection(projection);
 
         // Append paths for US-Mexico map
         migrationSvg.selectAll(".custom")
@@ -107,37 +159,35 @@ function setup() {
     })
     .catch(error => console.error('Error loading GeoJSON files:', error));
 
+    function updateMap(year) {
+        // Fetch GeoJSON data for the specified year
+        fetch(`${globalApplicationState.current_state}/plot_${globalApplicationState.current_species}_${year}.geojson`)
+            .then(response => response.json())
+            .then(birdData => {
+                // Remove existing bird observation paths
+                migrationSvg.selectAll(".bird-observation").remove();
+                
+                // Append paths for bird observation data
+                migrationSvg.selectAll(".bird-observation")
+                    .data(birdData.features)
+                    .enter().append("path")
+                    .attr("class", "bird-observation")
+                    .attr("d", pathGenerator)
+                    .style("fill", d => {
+                        if (d.properties.eargre === 'NA') {
+                            return 'none'; // No data, same color as background
+                        } else {
+                            const value = +d.properties.eargre; // Convert to number
+                            // Map values close to 0 to a color closer to the background
+                            return value < 0.01 ? d3.interpolate("#d3d3d3", customColorScale(value))(0.1) : customColorScale(value);
+                        }
+                    });
+            })
+            .catch(error => console.error('Error updating map:', error));
+    }
 
-        // Append SVG for the slider
-    const sliderSvg = body.append('svg')
-    .attr('id', 'sliderSvg')
-    .attr("height", pageHeight)
-    .attr("width", pageWidth / 2)
-    .style("position", "absolute") // Position absolutely
-    .style("top", "50px") // Adjust top position as needed
-    .style("left", "0px") // Position it on the left side
+    // Slider for months when needed below:
 
-    // Create the slider
-    const slider = d3.sliderHorizontal()
-    .min(2004)
-    .max(2023)
-    .step(1)
-    .width(pageWidth / 2.5) // Adjust the width as needed
-    .tickValues(d3.range(2004, 2024, 1)) // Include all years as tick values
-    .tickFormat((d) => d); // Tick values range from 2004 to 2023
-
-    // Append the slider to the slider SVG
-    sliderSvg.append('g')
-    .attr('transform', 'translate(60,510)') // Adjust positioning as needed
-    .attr("id", "slider_group")
-    .call(slider)
-    .attr('class', 'slider');
-
-    // Select all ticks and style them
-    sliderSvg.selectAll(".tick line")
-    .style("stroke-width", "2px") // Adjust stroke width as needed
-    .attr("y2", 10)
-    .style("stroke", "black");
 
     // const sliderSvg = body.append('svg')
     // .attr('id', 'sliderSvg')
